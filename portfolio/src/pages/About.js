b083efca89067a57b5719e096f40f28f5de6a484
jho0000 <jho0000rm@gmail.com>
2023-07-05 15:11:53 -0700

    Pages and some CSS.

---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+const About = () => {
+    return (
+    <div class="container">
+        <div class="content col-xs-5 col-md-6 m-2 p-6">
+            <p class="col-xs-5 col-md-12 m-4 p-6"></p>
+            <p class="col-xs-5 col-md-12 m-4 p-6"></p>
+            <p class="col-xs-5 col-md-12 m-4 p-6"></p>
+        </div>
+        <div class="content col-xs-6 col-md-5 m-4 d-flex justify-content-center">
+            <img className="aboutUs-image" src="img/JDH1.png" alt='Portrait'></img>
+        </div>
+    </div>
+    );
+};
+ 
+export default About;
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,27 @@
+<body>
+<header>
+  <div class="logo">
+    <img src="/img/JHlogo.png" alt="Logo">
+  </div>
+  <nav>
+    <ul class="menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+    </ul>
+    <a class="resume-button" href="resume.pdf" target="_blank">Resume</a>
+  </nav>
+</header>
+<footer>
+  <div class="footer-content">
+    <h1>John Ho · Des Moines, WA, USA</h1>
+    <h1 class="email">jho0000rm@gmail.com</h1>
+    <div class="icons">
+      <a href="https://www.linkedin.com/in/jho0000/"><img src="/img/Linkedin.png" alt="LinkedIn"></a>
+      <a href="https://github.com/jho0000"><img src="/img/Github.png" alt="GitHub"></a>
+      <a href="https://www.instagram.com/khoala16/"><img src="img/Instagram.png" alt="Instagram"></a>
+      <a href="https://discordapp.com/users/244531445555658752"><img src="/img/Discord.png" alt="Discord"></a>
+    </div>
+  </div>
+</footer>
+</body>

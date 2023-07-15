840e1405383954b4c003dc995e27639fe840764e
jho0000 <jho0000rm@gmail.com>
2023-07-15 15:10:59 -0700

    Minor page content.

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -2,16 +2,48 @@ import React from "react";
  
 function About () {
     return (
-    <div class="container">
-        <div class="content col-xs-5 col-md-6 m-2 p-6">
-            <p class="col-xs-5 col-md-12 m-4 p-6">Test Test</p>
-            <p class="col-xs-5 col-md-12 m-4 p-6">Test Test</p>
-            <p class="col-xs-5 col-md-12 m-4 p-6">Test </p>
+    <div class="container-fluid">
+        <div class='row'>
+            <div class="col-8">
+            <p>Hello and welcome! 👋</p>
+            <p>I’m John Ho, an aspiring UI UX researcher in Washington fueled by visual arts and music. I love designing cool projects, and love talking to people even more! :D</p>
+            <p>With a B.Sc. in Informatics focused on Human-Computer Interaction, I explore design and research processes to put into my projects and future works. I have experience conducting user research, creating design systems, and bridging design to developers in apps and design solutions.
+            With my love of the Internet and technology, I am inspired to promote well-being within information systems and create quality of life changes for all.</p>
+            </div>
         </div>
-        <div class="content col-xs-6 col-md-5 m-4 d-flex justify-content-center">
-            <img src="img/JDH1.png" alt='Portrait'></img>
+        <div class='row'>
+            <div class="col-md-4 col-xs-12 d-flex justify-content-center">
+                <img src="img/JDH1.png" alt='Portrait'></img>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-8">
+            <p>✨ Skills ✨</p>
+            <p>📝 UX Research 📝</p>
+            <p>💡 User Testing 💡</p>
+            <p>🎨 Design Systems 🎨</p>
+            <p>🔧 Wireframes and Prototypes 🔧</p>
+            <p>🌐 Web and UI Design 🌐</p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-4 col-xs-12 d-flex justify-content-center">
+                <img src="img/JDH2.png" alt='Portrait'></img>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-8">
+            <p>With a background in performing music and visual arts before university, I have always been interested in the complexity of human emotions and communication through art. I love drawing, music, and psychology, which lead to my passion in the UI UX field.</p>
+            <p>I also love video games, cooking, and diving into Internet subcultures. In my free time I am reading self-help books and manga, walking my two dogs, and learning new recipes in the kitchen -- all while listening to music.</p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-4 col-xs-12 d-flex justify-content-center">
+                <img src="img/JDH3.png" alt='Portrait'></img>
+            </div>
         </div>
     </div>
+    
     );
 };
  
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -3,8 +3,8 @@ function Projects () {
     return (
     <div class="container">
         <div class="content col-xs-5 col-md-6 m-2 p-6">
-            <p class="col-xs-5 col-md-12 m-4 p-6">Test Test</p>
-            <p class="col-xs-5 col-md-12 m-4 p-6">Test Test</p>
+            <p class="col-xs-5 col-md-12 m-4 p-6">Hello and welcome! 👋</p>
+            <p class="col-xs-5 col-md-12 m-4 p-6">I’m John Ho, an aspiring UI UX researcher in Washington fueled by visual arts and music. I love designing cool projects, and love talking to people even more! :D</p>
             <p class="col-xs-5 col-md-12 m-4 p-6">Test </p>
         </div>
         <div class="content col-xs-6 col-md-5 m-4 d-flex justify-content-center">

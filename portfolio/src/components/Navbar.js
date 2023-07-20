93035c3f6974182c6cef4dbb5b62c49be8e85310
jho0000 <jho0000rm@gmail.com>
2023-07-20 02:58:34 -0700

    Styling + need to fix header

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -5,10 +5,12 @@ return (
     <nav>
         <div class="navbar">
             <img src="img/JHlogo.png" alt="Logo"></img>
-            <NavLink to='/home'><h1>Home</h1></NavLink>
-            <NavLink to='/about'><h1>About</h1></NavLink>
-            <NavLink to='/projects'><h1>Projects</h1></NavLink>
-            <h1>Resume</h1>
+            <ul>
+                <li><NavLink to='/home'><h1>Home</h1></NavLink></li>
+                <li><NavLink to='/about'><h1>About</h1></NavLink></li>
+                <li><NavLink to='/projects'><h1>Projects</h1></NavLink></li>
+                <button type="button">Resume</button> 
+            </ul>
         </div>
     </nav>
     )

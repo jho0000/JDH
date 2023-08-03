e0fff1de88143c804597078fb9446de1229aa47f
jho0000 <jho0000rm@gmail.com>
2023-08-02 19:38:08 -0700

    Justified contents middle

--- portfolio/src/pages/ProjectPage.js
+++ portfolio/src/pages/ProjectPage.js
@@ -26,7 +26,7 @@ function ProjectPage () {
 
     return (
         <main>
-             <div class="banner fullscreen d-flex align-items-center justify-content-center">
+             <section class="banner page d-flex align-items-center justify-content-center">
                 <div class="banner card col-10">
                     <div class="row">
                         <div class="card col-md-6 justify-content-center color-1 left-banner">
@@ -38,8 +38,8 @@ function ProjectPage () {
                         </div>
                     </div>
                 </div>
-            </div>
-            <section class="page col-10">
+            </section>
+            <section class="page col-6">
                 <div class="summary row">
                     <div class="col-md-8">
                         <h1>Project Summary</h1>
@@ -74,7 +74,7 @@ function ProjectPage () {
                 <div class="background">
                     <h1>Background</h1>
                     <p>The current state of music discovery heavily relies on algorithmic methods, which have led to users feeling restricted or isolated in their music taste. There is innate gender imbalance and popularity biases within the algorithm system, shadowing diverse artists and creating a feedback loop within recommendations. There are many pain points about the algorithm we noted from user interviews and surveys:</p>
-                    <img src="/img/Moments/PainPointsH.png" alt="Pain Points"></img>
+                    <img class="asset" src="/img/Moments/PainPointsH.png" alt="Pain Points"></img>
                     <p>These initial insights lead to our problem statement:</p>
                 </div>
                 <div class="hmw-statement">
@@ -84,38 +84,38 @@ function ProjectPage () {
                 <div class="research">
                     <h1>Research Findings</h1>
                     <p>Literature research suggests that <u>organic music consumption</u> promotes more diverse music tastes, fostering a richer user experience and intimate connection with music recommendations. This concept was our stepping stone in our app design and addressing algorithmic flaws in music recommendations.</p>
-                    <img src="/img/Moments/Organic.png" alt="Organic music consumption"></img>
+                    <img class="asset" src="/img/Moments/Organic.png" alt="Organic music consumption"></img>
                     <p>From <b>28 survey respondents</b> and <b>10 interviews</b>, there is high interest in discovering new music to enjoy with the help of cross-referencing different apps (e.g. Spotify and YouTube). However, users encounter songs that they have already listened to in shuffled playlists or recommendations. We also highlighted that friends and online communities were a major source of music recommendations.</p>
                     <p>With <u>face-to-face music recommendations</u> being a memorable and effective way to share music, intimacy and familiarity enables finding enjoyable music from someone else. These social interactions allow people to connect and express their unique music taste intimately, offering a conversation for both musicianship and personal identity. With this, personal relationships and communities fuel discussions and enjoyment in sharing music, and the positive impacts of organic music consumption foster diverse music taste as a result.</p>
                 </div>
                 <div class="analysis">
                     <h1>Competitive Analysis</h1>
                     <p>Our competitors come from three major categories: Social Media for networking, Music Streaming for listening to music, and Music Discovery for finding new music.</p>
-                    <img src="/img/Moments/Competitors.png" alt="Moments' competitors"></img>
+                    <img class="asset" src="/img/Moments/Competitors.png" alt="Moments' competitors"></img>
                     <p class="caption">Categorization of explored competitors and case studies</p>
                     <p><b>Instagram</b> and <b>TikTok</b> are platforms where music can reach global status through virality, whereas <b>Reddit</b> and <b>last.fm</b> are examples of hubs where music is discovered. And with sharing music being as easy as sending a URL link or showing a song on your phone, established social media platforms were our main competitors in users encountering music quickly.</p>
                     <p><b>iTunes</b>, <b>Spotify</b>, and <b>YouTube Music</b> are the trilogy when it comes to streaming music. All three platforms have algorithms that select music and artists based on a mixture of listening history and popularity. Recent listening, songs that are most replayed, time spent on a playlist, liked and favorited songs, anything trending on Billboard or any music charts, it all gets compiled by the algorithm to give you <i>familiar</i> content. We saw this as a major pain point with using algorithmic suggestions to discover <i>new</i> music to enjoy.</p>
-                    <img src="/img/Moments/Algorithm.png" alt="YouTube Music and Spotify algorithmic recommendations"></img>
+                    <img class="asset" src="/img/Moments/Algorithm.png" alt="YouTube Music and Spotify algorithmic recommendations"></img>
                     <p class="caption">YouTube Music and Spotify curating recommendations based on my history</p>
                 </div>
                 <div>
                     <h2>Drawing Inspiration</h2>
                     <p>Seeing how social media plays a crucial role in connecting listeners and artists, our team examined features and successes of social media platforms to draw into our own app:</p>
-                    <img src="/img/Moments/Analysis.png" alt="Appeals to BeReal, Instagram, and Discord"></img>
+                    <img class="asset" src="/img/Moments/Analysis.png" alt="Appeals to BeReal, Instagram, and Discord"></img>
                     <p>With Discord syncing Spotify streaming to a profile’s status, and the new addition to Instagram having the option to put a song as a note, we knew other platforms were exploring interactions around music.</p>
-                    <img src="/img/Moments/StatusH.png" alt="Song statuses on Discord and Instagram"></img>
+                    <img class="asset" src="/img/Moments/StatusH.png" alt="Song statuses on Discord and Instagram"></img>
                     <p>This further validated the concept of <u>wanting to share music or seeing what others are listening to</u>.</p>
                 </div>
                 <div class="opportunities">
                     <h1>Opportunity Areas</h1>
                     <p>Moments was made to be <u>transparent</u> in who is recommending music, and let the users decide for themselves on the music they want to check out. There is no algorithm as well with shared songs being purely user-dependent, and people can sate their curiosity on what is being listened to by whom and where.</p>
-                    <img src="/img/Moments/Goals.png" alt="Goals of Moments"></img>
+                    <img class="asset" src="/img/Moments/Goals.png" alt="Goals of Moments"></img>
                     <p>Music is intimate, and we see Moments being an outlet for people to share that. We aim for authenticity in users’ recommended songs, not having to worry about appealing to others.</p>
                 </div>
                 <div class="personas">
                     <h1>Personas</h1>
                     <p>Based on our user research, we created a few user personas that demonstrated the pain points and motivations of finding new music to listen to.</p>
-                    <img src="/img/Moments/PersonasH.png" alt="Personas"></img>
+                    <img class="asset" src="/img/Moments/PersonasH.png" alt="Personas"></img>
                     <p>Whether it be close friends, the passionate fan, content creator, or knowledgeable musician, we envisioned Moments to let users explore both music <i>and</i> people with their music recommendations.</p>
                 </div>
                 <div class="design-process">
@@ -125,44 +125,44 @@ function ProjectPage () {
                     <div class="lofi-prototype">
                         <h2>Low-Fidelity Prototype</h2>
                         <p>In our user testing, we asked our participants to perform the following tasks to gauge usability and study their exploration:</p>
-                        <img src="/img/Moments/Tasks.png" alt="User testing tasks"></img>
-                        <p class="caption">Here were some of the workflows visualised from Figma:</p>
-                        <img src="/img/Moments/WF1.gif" alt="Low-fidelity prototype GIF of exploring local map page posts"></img>
+                        <img class="asset" src="/img/Moments/Tasks.png" alt="User testing tasks"></img>
+                        <p>Here were some of the workflows visualised from Figma:</p>
+                        <img class="asset" src="/img/Moments/WF1.gif" alt="Low-fidelity prototype GIF of exploring local map page posts"></img>
                         <p class="caption">Exploring local map page posts</p>
-                        <img src="/img/Moments/WF2.gif" alt="Low-fidelity prototype GIF of selecting a song to drop a Moment"></img>
+                        <img class="asset" src="/img/Moments/WF2.gif" alt="Low-fidelity prototype GIF of selecting a song to drop a Moment"></img>
                         <p class="caption">Selecting a song to drop a Moment</p>
-                        <img src="/img/Moments/WF3.gif" alt="Low-fidelity prototype GIF of a notifications page and exploring a profile"></img>
+                        <img class="asset" src="/img/Moments/WF3.gif" alt="Low-fidelity prototype GIF of a notifications page and exploring a profile"></img>
                         <p class="caption">Notifications page and exploring a profile</p>
                     </div>
                     <div>
                         <h2>Giving Direction</h2>
                         <p>Across <b>6 wireframe testers</b>, the average time to go through all the workflows was <b>5 minutes</b>. Since the interface was greyscale, the icons and buttons were ambiguous in navigating across pages. Specifically, the upper right pin icon was not clear in the action of searching up a song and dropping a Moment <i>until</i> they saw their pin isolated on the map. Additionally, the two magnifying glass icons also served different purposes, one to search location while the other was a general search query, which caused slight confusion.</p>
-                        <img src="/img/Moments/WF4.png" alt="Low-fidelity prototype image of duplicate icons"></img>
+                        <img class="asset" src="/img/Moments/WF4.png" alt="Low-fidelity prototype image of duplicate icons"></img>
                         <p class="caption">Icons had to be distinctive to avoid confusion</p>
                         <p>However, the onboarding process of making an account and using the navigation bar had familiarity to other social media apps. The notifications and profile pages were well-received with its information hierarchy, and our testers understood the intentions behind the prototype of exploring the map page and profiles.</p>
-                        <img src="/img/Moments/Testing.png" alt="Successes and pain points from user testing"></img>
+                        <img class="asset" src="/img/Moments/Testing.png" alt="Successes and pain points from user testing"></img>
                     </div>
                     <div class="hifi-prototype">
                         <h2>High-Fidelity Prototype</h2>
                         <p>With a refined prototype and more interactive features, the app cemented the importance of interacting with other users in many ways. The familiarity of conventions and interfaces contributed to an easier learning curve in exploring our app, and we made changes to the UI language for clarity.</p>
-                        <img src="/img/Moments/MF1.gif" alt="High-fidelity prototype GIF of exploring a post’s location and profile"></img>
+                        <img class="asset" src="/img/Moments/MF1.gif" alt="High-fidelity prototype GIF of exploring a post’s location and profile"></img>
                         <p class="caption">Exploring a post’s location and profile</p>
-                        <img src="/img/Moments/MF2.gif" alt="High-fidelity prototype GIF of editing profile and followers list"></img>
+                        <img class="asset" src="/img/Moments/MF2.gif" alt="High-fidelity prototype GIF of editing profile and followers list"></img>
                         <p class="caption">Editing profile and followers list</p>
-                        <img src="/img/Moments/MF3.gif" alt="High-fidelity prototype GIF of dropping a Moment on the map"></img>
+                        <img class="asset" src="/img/Moments/MF3.gif" alt="High-fidelity prototype GIF of dropping a Moment on the map"></img>
                         <p class="caption">Dropping a Moment on the map</p>
                         <p>Testers explored options to make their account public or private, setting their own posts to be exclusive to followers instead of the general public.</p>
                     </div>
                     <div>
                         <h2>Making the Live App</h2>
                         <p>I was primarily a tester for our developers to bugfix our Expo Go Simulator version of Moments. Currently, Moments is compatible for iOS devices so my testing can only be done on a MacBook. I documented bugs in the onboarding process, suggested styling changes (e.g. margin and spacing), and mapped out transitions between screens.</p>
-                        <img src="/img/Moments/HF1.gif" alt="Home map page"></img>
+                        <img class="asset" src="/img/Moments/HF1.gif" alt="Home map page"></img>
                         <p class="caption">Home map page</p>
-                        <img src="/img/Moments/HF2.gif" alt="Exploring someone’s Moment and profile"></img>
+                        <img class="asset" src="/img/Moments/HF2.gif" alt="Exploring someone’s Moment and profile"></img>
                         <p class="caption">Exploring someone’s Moment and profile</p>
-                        <img src="/img/Moments/HF3.gif" alt="Viewing an active Moment on the Moments page"></img>
+                        <img class="asset" src="/img/Moments/HF3.gif" alt="Viewing an active Moment on the Moments page"></img>
                         <p class="caption">Viewing an active Moment on the Moments page</p>
-                        <img src="/img/Moments/HF4.gif" alt="Dropping a Moment at my current location"></img>
+                        <img class="asset" src="/img/Moments/HF4.gif" alt="Dropping a Moment at my current location"></img>
                         <p class="caption">Dropping a Moment at my current location</p>
                     </div>
                 </div>
@@ -175,7 +175,7 @@ function ProjectPage () {
                         <li>I <b>facilitated project management</b> through organising pages upon pages of affinity maps, research insights, user validation, and compiled user research.</li>
                     </ul>
                     <p>Being involved in every stage of Moments was simply fulfilling for me, being able to apply what I have learned in design methods and UI / UX courses from university. The team submitted our project and demo video to our <a href="https://ischool.uw.edu/capstone/projects/2023/moments" target="_blank" rel="noopener noreferrer">university’s capstone website</a>, and now await for iOS TestFlight’s approval. Moving forward, the team is continuing discussions on the <u>app’s marketability</u>, <u>revenue strategies</u>, and <u>establishing branding</u> to commercialise our app.</p>
-                    <img src="/img/Moments/Preview.png" alt="Moments screens"></img>
+                    <img class="preview" src="/img/Moments/Preview.png" alt="Moments screens"></img>
                 </div>
                 <div class="conclusion">
                     <h1>Thanks for reading!</h1>

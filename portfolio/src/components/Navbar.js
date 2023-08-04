74fa3b5b352faaf84dbc428bc93cd1c090dbe74b
jho0000 <jho0000rm@gmail.com>
2023-08-04 01:03:22 -0700

    Finalized nav bar, need img and viewport styling

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,11 +4,11 @@ export function Navbar () {
 return (
     <nav>
         <div class="navbar">
-            <a class="logo" href="/home"><img src="/img/JHlogo.png" alt="Logo"></img></a>
+            <a href="/home"><img class="logo" src="/img/JHlogo.png" alt="Logo"></img></a>
             <ul class="nav align-items-center">
-                <li><NavLink to='/home'><h2>Home</h2></NavLink></li>
-                <li><NavLink to='/about'><h2>About</h2></NavLink></li>
-                <li><NavLink to='/projects'><h2>Projects</h2></NavLink></li>
+                <li><NavLink to='/home'><h3>Home</h3></NavLink></li>
+                <li><NavLink to='/about'><h3>About</h3></NavLink></li>
+                <li><NavLink to='/projects'><h3>Projects</h3></NavLink></li>
                 <a href="img/TestFile.pdf" target="_blank" rel="noopener noreferrer"><button type="button"><h2>Resume</h2></button></a>
             </ul>
         </div>

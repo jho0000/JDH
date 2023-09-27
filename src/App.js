5e65023d81e8ac5cbe8010fdd5a054f807cc570f
jho0000rm <jho0000rm@gmail.com>
2023-09-26 17:27:43 -0700

    Added home route.

--- src/App.js
+++ src/App.js
@@ -41,7 +41,8 @@ function App () {
         <div>
             <Navbar></Navbar>
             <Routes>
-                <Route path="/home" element={<Home/>}> </Route>
+                <Route path="" element={<Home/>}> </Route>
+                <Route path="home" element={<Home/>}> </Route>
                 <Route path="/about" element={<About/>}> </Route>
                 <Route path="/projects"> 
                     <Route path="" element={<Projects/>} />
--- src/pages/Musicroom.js
+++ src/pages/Musicroom.js
@@ -5,8 +5,7 @@ function Musicroom() {
         <main>
             <section id="about-me" class="fullscreen d-flex align-items-center justify-content-center">
                 <div class="home col-10">
-                    <h1 class="text-center">Name’s John, <span>welcome to my portfolio</span>!</h1>
-                    <h1 class="text-center">I'm a <span> UI / UX Researcher.</span></h1>
+                    <h1 class="text-center">Coming soon!</h1>
                 </div>
             </section>
         </main>

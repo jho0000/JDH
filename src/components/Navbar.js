4474796b8ba2124b0226a4b6bef2f527828166e0
jho0000rm <jho0000rm@gmail.com>
2023-09-28 01:09:36 -0700

    Resume file.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ return (
                 <li><NavLink to='/home'><h3>Home</h3></NavLink></li>
                 <li><NavLink to='/about'><h3>About</h3></NavLink></li>
                 <li><NavLink to='/projects'><h3>Projects</h3></NavLink></li>
-                <a href="img/TestFile.pdf" target="_blank" rel="noopener noreferrer"><button type="button"><h2>Resume</h2></button></a>
+                <a href="img/JohnHoResume2023.pdf" target="_blank" rel="noopener noreferrer"><button type="button"><h2>Resume</h2></button></a>
             </ul>
         </div>
     </nav>

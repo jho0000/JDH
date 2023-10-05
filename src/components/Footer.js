1725957b6dfbd593236f328651cb542771e01646
jho0000rm <jho0000rm@gmail.com>
2023-10-05 16:38:25 -0700

    Final edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ export function Footer () {
 return (
     <footer>
     <div class="footer-content">
-        <h2>John Ho · Des Moines, WA, USA</h2>
-        <h2>jho0000rm@gmail.com</h2>
+        <h2>John Ho · Des Moines, WA</h2>
+        <h2><a href="mailto:jho0000rm@gmail.com"><u>jho0000rm@gmail.com</u></a> · 206-460-2681</h2>
             <div class="icons">
+                <a href="mailto:jho0000rm@gmail.com"><img src="/img/Email.png" alt="Email" /></a>
                 <a href="https://www.linkedin.com/in/jho0000/"><img src="/img/Linkedin.png" alt="LinkedIn" /></a>
                 <a href="https://github.com/jho0000"><img src="/img/Github.png" alt="GitHub" /></a>
-                <a href="https://www.instagram.com/khoala16/"><img src="/img/Instagram.png" alt="Instagram" /></a>
-                <a href="https://discordapp.com/users/244531445555658752"><img src="/img/Discord.png" alt="Discord" /></a>
+                <a href="https://read.cv/jho0000"><img src="/img/Resume.png" alt="Resume" /></a>
             </div>
     </div>
     </footer>
